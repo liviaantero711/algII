ca171a93a6546b69d47834c5fe241b56300a3239
liviaantero711 <livia.antero@estudante.ifms.edu.br>
2025-09-23 17:40:28 -0400

    Add files via upload

---
+++ funcoes1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+/*Faça uma função que não receba parâmetros e escreva na tela (print) o
+texto “OI”.*/
+void palavra()
+{
+  printf("oi");
+}
+int main()
+{
+  palavra();
+  return(0);
+}

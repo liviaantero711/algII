1589e128439410cfd7c661e0854592d4506f8d9c
liviaantero711 <livia.antero@estudante.ifms.edu.br>
2025-11-17 07:44:16 -0400

    Add files via upload

---
+++ exercicio_12.c
@@ -0,0 +1,12 @@
+/*eu criei uma string e pedi para o usuario adicionar o nome confome o tamanho da string*/
+#include<stdio.h>
+/*12- faça um algoritmo com uma variável tipo char e defina o número de
+letras que a pessoa pode escrever*/
+int main(){
+  char a[5];
+  printf("escreva seu primeiro nome com o maximo 5 letras:");
+  scanf("%s",a);
+  printf("seu nome:%s",a);
+  return(0);
+}
+

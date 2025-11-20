79e9aa08494b8fa0b99b2fa352da98b42832f878
liviaantero711 <livia.antero@estudante.ifms.edu.br>
2025-11-20 15:43:40 -0400

    Add files via upload

---
+++ esc5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void converter(int r)
+{
+  int h,m,s;
+  h=r/3600;
+  r=r%3600;
+  m=r/60;
+  s=r%60;
+  printf("Horas: %d Minutos: %d Segundos: %d",h,m,s);
+}
+
+int main()
+{
+  int r
+  printf("Digite o tempo em segundos: ");
+  scanf("%d",&r);
+  converter(r);
+  return 0;
+}
